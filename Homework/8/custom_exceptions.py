b4407a7949c074771f7b50a5faba66eb290cc8dc
kilona57 <113191278+kilona57@users.noreply.github.com>
2024-05-23 17:45:20 +0300

    Delete Homework/8 directory

--- Homework/8/custom_exceptions.py
+++
@@ -1,9 +0,0 @@
-class UnknownOperationError(Exception):
-    """Ошибка при вводе неизвестной операции"""
-    def __str__(self) -> str:
-        return f"Ошибка: Введена неизвестная операция. Попробуй снова."
-
-class NotNumberError(Exception):
-    """Ошибка при вводе не числа"""
-    def __str__(self) -> str:
-        return f"Ошибка: Введено значение, которое не является числом. Попробуй снова."
